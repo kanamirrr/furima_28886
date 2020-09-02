843fc32fa4c224242e7d39605de78869b5d21690
kanamirrr <kaaaaanaaam@gmail.com>
2020-09-02 11:43:03 +0900

    モデルの単体テストコード

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :nickname, :birth_day])
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "full-width characters."}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "full-width characters."}
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters."}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters."}
+    validates :nickname
+    validates :birth_day
+    # validates :email
+    # validates :password, length: { minimum: 6 }
+    validates :password_confirmation
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: "include both letters and numbers." 
+    # validates :password, format: { with: PASSWORD_REGEX, message: "Password include both letters and numbers."}
+  end
 end
