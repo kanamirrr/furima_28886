a468e5002af987aed6c14e30d132881565725f08
kanamirrr <kaaaaanaaam@gmail.com>
2020-09-03 11:08:03 +0900

    Rubocopにてchek内容自動修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :nickname, :birth_day])
   end
@@ -12,8 +12,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    FULL_WIDTH_CHARACTER_REGEX = /\A[ぁ-んァ-ン一-龥]/
-    validates :family_name, format: { with: FULL_WIDTH_CHARACTER_REGEX, message: "full-width characters."}
-    validates :first_name, format: { with: FULL_WIDTH_CHARACTER_REGEX, message: "full-width characters."}
-    FULL_WIDTH_KANA_REGEX = /\A[ァ-ヶー－]+\z/
-    validates :family_name_kana, format: { with: FULL_WIDTH_KANA_REGEX, message: "full-width katakana characters."}
-    validates :first_name_kana, format: { with: FULL_WIDTH_KANA_REGEX, message: "full-width katakana characters."}
+    FULL_WIDTH_CHARACTER_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+    validates :family_name, format: { with: FULL_WIDTH_CHARACTER_REGEX, message: 'full-width characters.' }
+    validates :first_name, format: { with: FULL_WIDTH_CHARACTER_REGEX, message: 'full-width characters.' }
+    FULL_WIDTH_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+    validates :family_name_kana, format: { with: FULL_WIDTH_KANA_REGEX, message: 'full-width katakana characters.' }
+    validates :first_name_kana, format: { with: FULL_WIDTH_KANA_REGEX, message: 'full-width katakana characters.' }
     validates :nickname
     validates :birth_day
     validates :password_confirmation
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX, message: "include both letters and numbers." 
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'include both letters and numbers.'
   end
 end
