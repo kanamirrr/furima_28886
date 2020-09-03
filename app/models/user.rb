db7d7593a62e031430b3d41f1162b1ea2ff960e3
kanamirrr <kaaaaanaaam@gmail.com>
2020-09-03 12:29:40 +0900

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,6 @@ class User < ApplicationRecord
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX, message: 'include both letters and numbers.'
   end
+
 end
+
