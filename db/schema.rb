7a44d1666a0becc1615c672fda14810caee927f0
kanamirrr <kaaaaanaaam@gmail.com>
2020-09-04 08:42:57 +0900

    itemsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_054558) do
+ActiveRecord::Schema.define(version: 2020_09_03_060138) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,22 @@ ActiveRecord::Schema.define(version: 2020_09_03_054558) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.string "name", null: false
+    t.text "introduce", null: false
+    t.integer "category_id", null: false
+    t.integer "condition", null: false
+    t.integer "postage_payer", null: false
+    t.integer "prefecture_code", null: false
+    t.integer "preparation_day", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
     t.string "first_name", null: false
@@ -52,4 +68,5 @@ ActiveRecord::Schema.define(version: 2020_09_03_054558) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
