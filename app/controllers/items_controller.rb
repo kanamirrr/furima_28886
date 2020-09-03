f1caa9ecb73a750ebee063ba0278c030948422cd
kanamirrr <kaaaaanaaam@gmail.com>
2020-09-04 08:36:59 +0900

    Active Atrageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
+  def show
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :images).merge(user_id: current_user.id)
+  end
+
 end
---
+++ app/models/item.rb
@@ -0,0 +1,4 @@
+class Item < ApplicationRecord
+  has_many_attached :image
+
+end
