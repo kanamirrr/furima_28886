9e3a6cf55cbdd2c2dd8c8f19f494f1978823ac89
kanamirrr <kaaaaanaaam@gmail.com>
2020-09-03 10:45:16 +0900

    モデルの単体テストコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "full-width characters."}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "full-width characters."}
-    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters."}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters."}
+    FULL_WIDTH_CHARACTER_REGEX = /\A[ぁ-んァ-ン一-龥]/
+    validates :family_name, format: { with: FULL_WIDTH_CHARACTER_REGEX, message: "full-width characters."}
+    validates :first_name, format: { with: FULL_WIDTH_CHARACTER_REGEX, message: "full-width characters."}
+    FULL_WIDTH_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+    validates :family_name_kana, format: { with: FULL_WIDTH_KANA_REGEX, message: "full-width katakana characters."}
+    validates :first_name_kana, format: { with: FULL_WIDTH_KANA_REGEX, message: "full-width katakana characters."}
     validates :nickname
     validates :birth_day
     validates :password_confirmation
