ae783ad38b74c81bd2e1a3e6c32f286d14ad3040
kanamirrr <kaaaaanaaam@gmail.com>
2020-09-08 11:03:18 +0900

    出品情報をDBに保存するための実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,6 +11,7 @@ class ItemsController < ApplicationController
 
   def create
     Item.create(item_params)
+    redirect_to root_path
   end
 
   def show
@@ -19,7 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :images)
+    params.require(:item).permit(:image, :name, :introduce, :category_id, :condition_id, :postage_payer_id, :prefecture_code_id, :preparation_day_id, :price).merge(user_id: current_user.id)
   end
 
   def move_to_signin
