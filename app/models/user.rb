ec02113225c8e160ee62304f8e092db65f7770c6
kanamirrr <kaaaaanaaam@gmail.com>
2020-09-10 16:20:18 +0900

    Rubocopにてchek内容自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,4 @@ class User < ApplicationRecord
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX, message: 'include both letters and numbers.'
   end
-
 end
-
