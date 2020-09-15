3a77fca55f1b3a2b8d9c57eb928f179500bcc80f
kanamirrr <kaaaaanaaam@gmail.com>
2020-09-15 12:20:29 +0900

    アソシエーションの記述、追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :preparation_day
   has_one_attached :image
   belongs_to :user
+  has_one :purchaser
 
   with_options presence: true do
     validates :image
