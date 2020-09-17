bee5ee590ceb5c1f895f34f770b50660b036e9bf
kanamirrr <kaaaaanaaam@gmail.com>
2020-09-17 20:57:13 +0900

    purchasersのアクション定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
 
-  resources :items
+  resources :items do
+    resources :purchasers, only: [:index, :create, :new]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
