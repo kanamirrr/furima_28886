552e8f3e2c1fc0264e29de3ca4a89d8fa328a30b
kanamirrr <kaaaaanaaam@gmail.com>
2020-09-17 21:08:17 +0900

    モデルの作成とAssociation等の記述

--- app/models/purchaser.rb
+++ app/models/purchaser.rb
@@ -1,3 +1,8 @@
 class Purchaser < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture_code
   belongs_to :purchaser_info
+
+  attr_accessor :token
+
 end
