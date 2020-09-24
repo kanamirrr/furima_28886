8636d9be3fbedd308ea1d4b3b628fb6ead34172f
kanamirrr <kaaaaanaaam@gmail.com>
2020-09-24 18:44:53 +0900

    paramsの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,7 +44,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :introduce, :category_id, :condition_id, :postage_payer_id, :prefecture_code_id, :preparation_day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :introduce, :category_id, :condition_id, :postage_payer_id, :prefecture_code_id, :preparation_day_id, :price, images: []).merge(user_id: current_user.id)
   end
 
   def move_to_signin
